2859d8f8716c5a6a20ef3c816a4037322f0bf49c
Jeremiah King <usaxray25@gmail.com>
2022-06-14 21:24:00 -0500

    removed duplicate code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,4 @@
 // USE WITH FIREBASE AUTH
-// import checkLoginStatus from './helpers/auth';
-import 'bootstrap'; // import bootstrap elements and js
-import '../styles/main.scss';
-
-// USE WITH FIREBASE AUTH
 import checkLoginStatus from './helpers/auth';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
