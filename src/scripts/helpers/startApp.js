e436b6f79db62a5abf158f1a3035bf39c6e8adc1
Jeremiah King <usaxray25@gmail.com>
2022-06-16 20:58:20 -0500

    cards on dom

--- src/scripts/helpers/startApp.js
+++ src/scripts/helpers/startApp.js
@@ -1,31 +1,21 @@
-import { getOrders } from '../api/orderData';
+// import { getOrders } from '../api/orderData';
 import domBuilder from '../components/domBuilder';
 import navbar from '../components/pages/navBar';
 import navbarEvents from '../events/navbarEvents';
 import logoutButton from '../components/auth/logoutButton';
-<<<<<<< HEAD
 import domEvents from '../events/domEvents';
-=======
 import homeLoggedIn from '../components/pages/homeLoggedIn';
-// import domEvents from '../events/domEvents';
->>>>>>> main
 // import formEvents from '../events/formEvents';
-import { showOrders } from '../components/pages/orders';
+// import { showOrders } from '../components/pages/orders';
 
 const startApp = (user) => {
   domBuilder();// BUILD THE DOM
   navbar();
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
-<<<<<<< HEAD
+  homeLoggedIn(user);
   domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
   navbarEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
-=======
-  homeLoggedIn(user);
-  navbarEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
-  //   domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
->>>>>>> main
   //   formEvents(); // ADD FORM EVENT LISTENTERS TO THE DOM
-  getOrders().then((ordersArray) => showOrders(ordersArray));
 };
 
 export default startApp;
