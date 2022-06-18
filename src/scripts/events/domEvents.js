1e0336fa8c9ba92d61571ed5a4921a05ca47207a
Jeremiah King <usaxray25@gmail.com>
2022-06-18 09:03:10 -0500

    added card borders for viewing pleasure

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -2,7 +2,7 @@ import { deleteOrders } from '../api/orderData';
 import { showOrders } from '../components/pages/orders';
 
 const domEvents = () => {
-  document.querySelector('#main-container').addEventListener('click', (e) => {
+  document.querySelector('#view').addEventListener('click', (e) => {
     if (e.target.id.includes('delete-order')) {
       // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
