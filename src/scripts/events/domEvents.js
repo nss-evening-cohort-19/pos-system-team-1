faa52763e0a94390947ecc3ddc492fab3ef744ea
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-06-18 10:16:40 -0500

    home buttons in progress

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -1,6 +1,8 @@
 import { deleteOrders, getOrders } from '../api/orderData';
 import { showOrders } from '../components/pages/orders';
 import viewOrders from '../components/pages/viewOrders';
+import createOrderForm from '../components/forms/createAnOrderForm';
+import renderRevenue from '../components/pages/revenue';
 
 const domEvents = () => {
   document.querySelector('#view').addEventListener('click', (e) => {
@@ -12,10 +14,17 @@ const domEvents = () => {
       }
     }
   });
-  document.querySelector('#view')
-    .addEventListener('click', () => {
+  document.querySelector('#view').addEventListener('click', (e) => {
+    if (e.target.id.includes('viewOrderBtn')) {
       getOrders().then((orderArray) => viewOrders(orderArray));
-    });
+    }
+    if (e.target.id.includes('createOrderBtn')) {
+      createOrderForm();
+    }
+    if (e.target.id.includes('viewRevBtn')) {
+      renderRevenue();
+    }
+  });
 };
 
 export default domEvents;
--- src/scripts/events/navbarEvents.js
+++ src/scripts/events/navbarEvents.js
@@ -2,17 +2,23 @@
 import createOrderForm from '../components/forms/createAnOrderForm';
 import { getOrders } from '../api/orderData';
 import viewOrders from '../components/pages/viewOrders';
+import homeLoggedIn from '../components/pages/homeLoggedIn';
 
 // nav bar events
-const navbarEvents = () => {
+const navbarEvents = (uid) => {
   // view order button click
-  document.querySelector('#viewOrders')
-    .addEventListener('click', () => {
-      getOrders().then((orderArray) => viewOrders(orderArray));
-    });
+  document.querySelector('#viewOrders').addEventListener('click', () => {
+    getOrders(uid).then((orderArray) => viewOrders(orderArray));
+  });
 
   // create order button click
-  document.querySelector('#createOrder').addEventListener('click', createOrderForm);
+  document.querySelector('#createOrder').addEventListener('click', () => {
+    createOrderForm();
+  });
+
+  document.querySelector('#navbarTitle').addEventListener('click', () => {
+    homeLoggedIn(uid);
+  });
 };
 
 export default navbarEvents;
