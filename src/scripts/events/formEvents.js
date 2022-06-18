ea0495f06c4719173207ac5fdb40c0c89bc02f67
nishaya raj <nishayaraj@gmail.com>
2022-06-18 12:35:54 -0500

    create order issues

--- src/scripts/events/formEvents.js
+++ src/scripts/events/formEvents.js
@@ -1,10 +1,35 @@
-// const formEvents = (uid) => {
-//   document.querySelector('#main-container').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     if (e.target.dispatchEvent.includes('submit-card')) {
-//       const orderObject = {
+import { createOrder, updateOrder } from '../api/orderData';
+import { showOrders } from '../components/pages/showOrders';
 
-//       }
-//     }
-//   });
-// };
+const formEvents = () => {
+  document.querySelector('#main-container').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (e.target.id.includes('submit-order')) {
+      const orderObject = {
+        orderName: document.querySelector('#orderName').value,
+        customerPhone: document.querySelector('#phone').value,
+        customerEmail: document.querySelector('#email').value,
+        orderType: document.querySelector('').value
+      };
+
+      createOrder(orderObject).then((ordersArray) => console.warn(ordersArray));
+    }
+
+    // TODO: CLICK EVENT FOR EDITING A Card
+    if (e.target.id.includes('update-order')) {
+      const [, orderFirebaseKey] = e.target.id.split('--');
+      const orderObject = {
+        orderName: document.querySelector('#orderName').value,
+        customerPhone: document.querySelector('#phone').value,
+        customerEmail: document.querySelector('#email').value,
+        orderType: document.querySelector('').value,
+        orderFirebaseKey
+      };
+
+      updateOrder(orderObject).then(showOrders);
+    }
+  });
+};
+
+export default formEvents;
