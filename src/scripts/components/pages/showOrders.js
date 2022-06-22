1c1b828c45ff8bc2ad6dc3d577abe971b987e4da
Jeremiah King <usaxray25@gmail.com>
2022-06-21 20:48:11 -0500

    adding better buttons

--- src/scripts/components/pages/showOrders.js
+++ src/scripts/components/pages/showOrders.js
@@ -10,17 +10,17 @@ const showOrders = (array) => {
 
   if (array.length) {
     let domString = '';
-    array.forEach((item) => {
+    array.forEach((obj) => {
       domString += `<div class="orders-card">
       <div class="card-body">
-      <h5 class="card-title">${item.orderName}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">${item.closedStatus}</h6>
-      <p class="card-phone">${item.customerPhone}</p>
-      <p class="card-date">${item.customerEmail}</p>
-      <p class="card-type">${item.orderType}</p>
-      <a href="#" class="link-success" id="details-order--${item.firebaseKey}">Details</a>
-      <a href="#" class="link-warning" id="edit-order--${item.firebaseKey}">Edit</a>
-      <a href="#" class="link-danger" id="delete-order--${item.firebaseKey}">Delete</a>
+      <h5 class="card-title">${obj.orderName}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${obj.closedStatus}</h6>
+      <p class="card-phone">${obj.customerPhone}</p>
+      <p class="card-date">${obj.customerEmail}</p>
+      <p class="card-type">${obj.orderType}</p>
+      <i class="btn btn-success fas fa-eye" id="details-order--${obj.firebaseKey}"></i>
+      <i class="fas fa-edit btn btn-warning" id="edit-order--${obj.firebaseKey}"></i>
+      <i class="btn btn-danger fas fa-trash-alt" id="delete-order--${obj.firebaseKey}"></i>
       </div>
     </div>
     <br>`;
