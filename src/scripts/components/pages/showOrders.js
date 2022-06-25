d27bf6b919ea3d29e095612bb4364c5fe6f9e9e3
Jeremiah King <usaxray25@gmail.com>
2022-06-24 21:13:01 -0500

    cards styled a little

--- src/scripts/components/pages/showOrders.js
+++ src/scripts/components/pages/showOrders.js
@@ -35,7 +35,7 @@ const filterButtonStr = `
   </div>`;
 
 const emptyOrders = () => {
-  const emptyOrderDom = `<div>${filterButtonStr}<div style='display: flex;flex-wrap: wrap;justify-content: center;padding: 30px;'>No item</div></div>`;
+  const emptyOrderDom = `<div>${filterButtonStr}<div style='display: flex;flex-wrap: wrap;justify-content: center;padding: 30px;'><h1>No Orders</h1><div><div>`;
 
   renderToDOM('#view', emptyOrderDom);
 };
@@ -48,11 +48,12 @@ const showOrders = (array) => {
     array.forEach((obj) => {
       domString += `<div class="orders-card">
       <div class="card-body">
-      <h5 class="card-title">${obj.orderName}</h5>
+      <h5 style="font-weight: bold; class="card-title">${obj.orderName}</h5><hr>
       <h6 class="card-subtitle mb-2 text-muted">Order Status: ${obj.closedStatus ? 'Closed' : 'Open'}</h6>
       <p class="card-phone">${obj.customerPhone}</p>
       <p class="card-date">${obj.customerEmail}</p>
       <p class="card-type">${obj.orderType}</p>
+      <p style="font-weight: bold; class="card-date">${obj.orderDate}</p>
       <i class="btn btn-success fas fa-eye"  id="details-order--${obj.firebaseKey}"></i>
       ${obj.closedStatus ? `<i class="fas fa-edit btn btn-info" style='color: grey;text-decoration: none;' id="done-order--${obj.firebaseKey}"></i>` : `<i class="fas fa-edit btn btn-info" id="edit-order--${obj.firebaseKey}"></i>`}
       <i class="btn btn-danger fas fa-trash-alt" id="delete-order--${obj.firebaseKey}"></i>
